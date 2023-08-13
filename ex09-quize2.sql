5bd94408d5d82d2031e30ce7087efe8e397d6406
FiA2906 <139411779+FiA2906@users.noreply.github.com>
2023-08-13 20:11:17 +0900

    00

--- ex09-quize2.sql
+++ ex09-quize2.sql
@@ -1,5 +1,11 @@
 -- 1. 각 직원의 성(last_name)과 해당 직원의 매니저인 직원의 성(last_name) 조회하기
+SELECT e.last_name, m.last_name
+FROM employees e
+JOIN employees m ON e.manager_id = m.employee_id;
 -- 2. 각 직원의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
+SELECT e.last_name, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id;
 -- 3. 각 부서의 이름(department_name)과 해당 부서의 평균 급여(avg_salary) 조회하기
 -- 4. 각 부서의 이름(department_name)과 해당 부서의 최대 급여(max_salary) 조회하기
 -- 5. 각 직원의 성(last_name)과 해당 직원이 속한 부서의 최소 급여(min_salary) 조회하기
